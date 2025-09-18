449f8a1b3ab1052aadf03fd84603779e413108aa
SharlynBernice <sharlyn10e@gmail.com>
2025-09-18 10:19:29 +0530

    Update word search in Matrix.java

--- word search in Matrix.java
+++ word search in Matrix.java
@@ -2,30 +2,21 @@ import java.util.*;
 
 public class Hello {
 
-    private static boolean dfs(int i, int j, char[][] grid, int r, int c, String str, int idx, boolean[][] visited) {
-        if (idx == str.length()) {
-            return true;
-        }
-        if (i < 0 || i >= r || j < 0 || j >= c) {
-            return false;
-        }
-        if (visited[i][j] || grid[i][j] != str.charAt(idx)) {
-            return false;
-        }
+    private static boolean dfs(int i, int j, char[][] grid, int r, int c, String str, int idx) {
+        if (idx == str.length()) return true;
+        if (i < 0 || j < 0 || i >= r || j >= c) return false;
+        if (grid[i][j] != str.charAt(idx)) return false;
 
-        visited[i][j] = true;
+        char ch = grid[i][j];
+        grid[i][j] = '.'; // mark as visited
 
         boolean found =
-                dfs(i, j + 1, grid, r, c, str, idx + 1, visited) ||  // right
-                dfs(i, j - 1, grid, r, c, str, idx + 1, visited) ||  // left
-                dfs(i + 1, j, grid, r, c, str, idx + 1, visited) ||  // down
-                dfs(i - 1, j, grid, r, c, str, idx + 1, visited) ||  // up
-                dfs(i + 1, j + 1, grid, r, c, str, idx + 1, visited) ||  // down-right
-                dfs(i + 1, j - 1, grid, r, c, str, idx + 1, visited) ||  // down-left
-                dfs(i - 1, j + 1, grid, r, c, str, idx + 1, visited) ||  // up-right
-                dfs(i - 1, j - 1, grid, r, c, str, idx + 1, visited);    // up-left
+            dfs(i + 1, j, grid, r, c, str, idx + 1) || // down
+            dfs(i - 1, j, grid, r, c, str, idx + 1) || // up
+            dfs(i, j + 1, grid, r, c, str, idx + 1) || // right
+            dfs(i, j - 1, grid, r, c, str, idx + 1);   // left
 
-        visited[i][j] = false; // backtrack
+        grid[i][j] = ch; // backtrack
         return found;
     }
 
@@ -34,31 +25,28 @@ public class Hello {
         int r = z.nextInt();
         int c = z.nextInt();
         char[][] grid = new char[r][c];
+
         for (int i = 0; i < r; i++) {
+            String s = z.next();
             for (int j = 0; j < c; j++) {
-                grid[i][j] = z.next().charAt(0);
+                grid[i][j] = s.charAt(j);
             }
         }
-        z.nextLine(); // consume newline
+
+        z.nextLine(); // consume leftover newline
         String str = z.nextLine();
 
         boolean flag = false;
-        boolean[][] visited = new boolean[r][c];
-
         for (int i = 0; i < r && !flag; i++) {
             for (int j = 0; j < c && !flag; j++) {
                 if (grid[i][j] == str.charAt(0)) {
-                    if (dfs(i, j, grid, r, c, str, 0, visited)) {
+                    if (dfs(i, j, grid, r, c, str, 0)) {
                         flag = true;
                     }
                 }
             }
         }
 
-        if (flag) {
-            System.out.println("yes");
-        } else {
-            System.out.println("no");
-        }
+        System.out.println(flag ? "yes" : "no");
     }
 }
